7021e0a793d2cc01d1ab5d629dd7c2afe4feb6a1
wk-mia <ry_always@163.com>
2020-08-27 14:47:50 +0800

    文档目录

---
+++ web/src/main/java/com/com/aoligei/remoter/RemoterApplication.java
@@ -0,0 +1,12 @@
+package com.com.aoligei.remoter;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RemoterApplication.class,args);
+    }
+}
